2317e6c4995ea1347495468b84dc2ad3ccf4388c
januario86 <januario86fp@gmail.com>
2020-08-01 10:27:53 -0300

    Criando consultas com JPQL

---
+++ src/main/java/br/com/januario/jpa/testes/TesteJPQL.java
@@ -0,0 +1,36 @@
+package br.com.januario.jpa.testes;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import br.com.januario.jpa.modelo.Movimentacao;
+
+public class TesteJPQL {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+		
+		String jpql = "select m from Movimentacao m where m.conta.id = 2";
+		
+		Query query = em.createQuery(jpql);
+		List<Movimentacao> resultList = query.getResultList();
+		
+		for (Movimentacao movimentacao : resultList) {
+			System.out.println("Descrição: " + movimentacao.getDescricao());
+			System.out.println("Tipo: " + movimentacao.getTipoMovimentacao());
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
