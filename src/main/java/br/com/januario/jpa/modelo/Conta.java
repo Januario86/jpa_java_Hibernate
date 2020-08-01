cb13e11339a97acd7ba97f6339f792f858883ab6
januario86 <januario86fp@gmail.com>
2020-08-01 06:10:23 -0300

    Inserindo dados testes para a classe conta

--- src/main/java/br/com/januario/jpa/modelo/Conta.java
+++ src/main/java/br/com/januario/jpa/modelo/Conta.java
@@ -1,11 +1,15 @@
 package br.com.januario.jpa.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Conta {
+	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	private Integer agencia;
